6f85e1e68afa3425aa57485cd681c0752606408e
CSUMB-Arthur <38265500+CSUMB-Arthur@users.noreply.github.com>
2018-11-05 21:44:10 -0800

    All files for hw2

---
+++ inc/functions.php
@@ -0,0 +1,187 @@
+<?php
+    function getNeighboringIndexes($index, $width, $height){
+        $validindexes = array();
+    	$validindexes[] = $index-$width-1;
+    	$validindexes[] = $index-$width+1;
+    	$validindexes[] = $index+$width-1;
+    	$validindexes[] = $index+$width+1;
+	    
+    	if (($index/$width)%2 == 0){ //horizontal
+    		if ($index%$width - 2 > 0){
+    			$validindexes[] = $index - 2;
+    		}
+    		if ($index%$width + 2 < $width){
+    			$validindexes[] = $index + 2;
+    		}
+    	}
+    	else{ //vertical
+    		if ($index/$width - 2 > 0){
+    			$validindexes[] = $index - 2*$width;
+    		}
+    		if ($index/$width + 2 < $height){
+    			$validindexes[] = $index + 2*$width;
+    		}
+    	}
+	    $validindexes[] = count($validindexes); //append maxindex to the end, so it can be retrieved and used.
+        return $validindexes;
+    }
+    
+    function GenerateMaze($width, $height){
+        $maxindex = $width * $height;
+        $mazearray = array_fill(0, $width * $height, 0);
+        $validstarts = array_fill(0, $width * $height, 0);
+        $validstartsindex = 0;
+        $newstart = true;
+        $currentindex = 0;
+        $prevoptionsindex = 0;
+        $prevoptions = array_fill(0, 5, 0);
+        $debugcounter = 0;
+        
+        //Init starting walls on all sides
+        for($i = 0; $i < $width; $i++){
+            $mazearray[$i] = 1;
+            $mazearray[($width*($height-1))+$i] = 1;
+        }
+        
+        for($i = 0; $i < $height; $i++){
+            $mazearray[$i*$width] = 1;
+            $mazearray[$i*$width + $width-1] = 1;
+        }
+        
+        //Init valid start points
+        
+        for ($i = 1; $i < ($width)/2; $i++){
+    		$mazearray[$i*2+1*$width] = 3;
+    		$validstarts[$validstartsindex++] = $i*2+1*$width;
+    		
+    		$mazearray[$i*2+($height-3)*$width + $width*($height%2)] = 3;
+    		$validstarts[$validstartsindex++] = $i*2+($height-3)*$width + $width*($height%2);
+    	}
+    	
+    	for ($i = 1; $i < ($height-1)/2; $i++){
+    		$mazearray[$i*2*$width + 1] = 3;
+    		$validstarts[$validstartsindex++] = $i*2*$width + 1;
+    		
+    		$mazearray[($i*2)*$width + $width - 3 +$width%2]= 3;
+    		$validstarts[$validstartsindex++] = ($i*2)*$width + $width - 3 +$width%2;
+    	}
+    	
+    	//Main loop
+    	while ($validstartsindex > 0 and $debugcounter < $maxindex and true){
+    	    //echo $debugcounter + " ";
+    	    $debugcounter++;
+    	    //echo $validstartsindex . ' ' ;
+    	    
+            if ($newstart == true){
+				$r = rand(0,$validstartsindex-1);
+				$currentindex = $validstarts[$r];
+				$validstarts[$r] = $validstarts[--$validstartsindex];
+				$newstart = false;
+			}
+			//Set chosen index (either from new start, or previous iteration's selection) to a wall.
+			$mazearray[$currentindex] = 1;
+			if (($currentindex/$width)%2 == 0){
+				$mazearray[$currentindex+1] = 1;
+				$mazearray[$currentindex-1] = 1;
+			}
+			else{
+				$mazearray[$currentindex+$width] = 1;
+				$mazearray[$currentindex-$width] = 1;
+			}
+				
+			$validoptions = getNeighboringIndexes($currentindex,$width,$height);
+            $validoptionsindex = array_pop($validoptions);
+			
+			for ($i = 0; $i < $validoptionsindex; ){
+				switch ($mazearray[$validoptions[$i]]){
+					//Set all white options to gold, and track
+					case 0:
+						$mazearray[$validoptions[$i]] = 4;
+					case 4:
+						$i++;
+						break;
+					
+					//Blue options, remove and set to red. Also find the matching validstarts[] index, and remove that entry
+					case 3:
+						$mazearray[$validoptions[$i]] = 2;
+						for ($j = 0;  $j < $validstartsindex; $j++){
+							if ($validoptions[$i] == $validstarts[$j]){
+								$validstarts[$j] = $validstarts[--$validstartsindex];
+								break;
+							}
+						}
+					//Wall, Red, and Blue options - remove.
+					case 1:
+					case 2:
+						$validoptions[$i] = $validoptions[--$validoptionsindex];
+						break;
+				}
+			}
+
+			
+			//select an option
+			if ($validoptionsindex > 0){
+				$r = rand(0,$validoptionsindex-1);
+				$currentindex = $validoptions[$r];
+			}
+			else{
+				$newstart = true;
+			}
+			
+
+
+			$found;
+			for ($i = 0; $i < $prevoptionsindex;){
+				$found = 0;
+				for ($j = 0; $j < $validoptionsindex; $j++){
+					if ($prevoptions[$i] == $validoptions[$j]){
+						$found = 1;
+						break;
+					}
+				}
+				if ($found == 1){
+					$prevoptions[$i] = $prevoptions[--$prevoptionsindex];
+				}
+				else{
+					$i++;
+				}
+			}
+			
+			for ($i = 0; $i < $prevoptionsindex; $i++){
+				//convert gold to blue, add as valid starts
+				if ($mazearray[$prevoptions[$i]] == 4){
+					$mazearray[$prevoptions[$i]] = 3;
+					$validstarts[$validstartsindex++] = $prevoptions[$i];
+				}
+			}
+			
+			$prevoptionsindex = 0;
+			for ($i = 0; $i < $validoptionsindex; $i++){
+				$prevoptions[$i] = $validoptions[$i];
+				$prevoptionsindex++;
+			}
+    	}
+    	
+    	//Convert values to 0-1
+    	for ($i = 0; $i < $maxindex; $i++){
+    		if ($mazearray[$i] > 1){
+    			$mazearray[$i] = 0;
+    		}
+    	}
+	    echo '<br/>';
+	    echo "<div id ='maze'>";
+    	for ($i = 0; $i < $height; $i++){
+    	    for ($j = 0; $j < $width; $j++){
+    	        if ($mazearray[$i*$width + $j] == 0){
+    	            echo "<img class='w' src='img/W.png'/>";
+    	        }
+    	        else{
+    	            echo "<img src='img/B.png'/>";
+    	        }
+    	    }
+    	    echo '<br/>';
+    	}
+        echo "</div>";
+    }
+    
+?>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Maze Generator </title>
+        <?php
+            include 'inc/functions.php';
+        ?>
+        
+        <link href="css/styles.css" rel ="stylesheet" type="text/css"/>
+        
+    </head>
+    <body>
+        <header>
+            <h1>Maze Generator</h1>
+        </header>
+        
+        <div id="main">
+            <div>
+                Generates mazes via some algorithm I came up with and ported to PHP. <br/>
+                Guarantees solvability, and that every tile is vistiable. <br/>
+                <a href="files/MazeC.txt">C code</a><br/>
+                <a href="files/MazeAuto.exe">Windows Executable (Automatic iteration)</a><br/>
+                <a href="files/MazeManual.exe">Windows Executable (Manual iteration)</a><br/>
+                <a href="files/MazePHP.txt">PHP code</a><br/>
+                
+            </div>
+            <div id="inputs">
+                <form id = "controls" method="post" action="index.php">
+                    <label>Height:</label>
+                    <input type="number" alt = "height" name="height" min="5" max="100" step="1"><br/>
+                    <label>Width:</label> 
+                    <input type="number" name="width" min="5" max="100" step="1">
+                    <br/><input type="submit" value="Generate">
+                </form>
+            </div>
+                <?php
+                    $width= $_POST["width"];
+                    $height= $_POST["height"];
+                    if ($width < 5){
+                        $width = 5;
+                    }
+                    if ($height < 5){
+                        $height = 5;
+                    }
+                    GenerateMaze($width, $height)
+                ?>
+        </div>
+    </body>
+            
+</html>
